44303c7bd5bc2fad967f91a49c178da8d0b7de69
Yonatan Reicher <yony252525@gmail.com>
2025-01-08 10:50:40 +0200

    Implement DefaultRunner::run

--- src/runner.rs
+++ src/runner.rs
@@ -1,6 +1,13 @@
-use std::{io::{self, Write}, path::{Path, PathBuf}, sync::Mutex, time::Duration};
 use crate::fuzzer::Fuzzer;
+use crate::stoppable_loop::{LoopAction, StoppableLoop};
+use shared_child::SharedChild;
 use std::thread;
+use std::{
+    io::{self, Write},
+    path::{Path, PathBuf},
+    sync::Arc,
+    time::Duration,
+};
 
 #[derive(Debug)]
 pub struct ProgramResult {
@@ -34,47 +41,83 @@ impl<T: Fuzzer> DefaultRunner<T> {
 
 /// Spawn a process with the given executable path and write the input to its
 /// stdin.
-fn spawn_with_stdin(executable: &Path, input: &[u8]) -> io::Result<std::process::Child> {
+fn spawn_with_stdin(executable: &Path, input: &[u8]) -> io::Result<SharedChild> {
     // TODO: Can we limit Command::new to only absolute paths?
-    let mut child = std::process::Command::new(executable)
-        .stdin(std::process::Stdio::piped())
-        .spawn()?;
-    child.stdin.as_mut()
-        .ok_or(io::Error::new(io::ErrorKind::Other, "could not open stdin pipe"))?
+    let child = SharedChild::spawn(
+        std::process::Command::new(executable)
+            .stdin(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::null()),
+    )?;
+    child
+        .take_stdin()
+        .as_mut()
+        .ok_or(io::Error::new(
+            io::ErrorKind::Other,
+            "could not open stdin pipe",
+        ))?
         .write_all(input)?;
     Ok(child)
 }
 
-impl<T: Fuzzer> Runner for DefaultRunner<T> {
-    fn run(&mut self) {
-        // TODO: Here we want to run over and over again, right? And return some metrics?
-        
-        let mut stop = false;
-        let mut last_child: Mutex<Option<std::process::Child>> = None.into();
-        let t = thread::spawn(move || {
-            while !stop {
-                let input = self.fuzzer.generate_input();
-                // TODO: Switch to '?' instead of unwrap
-                last_child;
-                child.wait();
-            }
-        });
+fn delay(delay: Duration, action: impl FnOnce() + Send + 'static) {
+    thread::spawn(move || {
+        thread::sleep(delay);
+        action();
+    });
+}
 
-        thread::sleep(self.timeout);
+struct RunnerLoopAction<'path, 'fuzzer, Fuzzer> {
+    fuzzer: &'fuzzer mut Fuzzer,
+    executable: &'path Path,
+}
 
-        while !stop { }
-        match self.run_with_input(&input) {
-            Ok(result) => println!("Execution succeeded: {:?}", result),
-            Err(e) => eprintln!("Execution failed: {:?}", e),
+impl<'p, 'f, F: Fuzzer> LoopAction for RunnerLoopAction<'p, 'f, F> {
+    type Stop = Arc<SharedChild>;
+    type Wait = (Arc<SharedChild>, String);
+
+    type Output = String;
+
+    fn stop(child: &Self::Stop) {
+        child.kill().expect("could not kill child process");
+    }
+
+    fn wait(child_and_input: Self::Wait) -> Option<Self::Output> {
+        let (child, input) = child_and_input;
+        let exit_status = child.wait().expect("could not wait for child process");
+        if exit_status.success() {
+            None
+        } else {
+            Some(input)
         }
     }
 
-    fn run_with_input(&mut self, input: &str) -> Result<ProgramResult, String> {
+    fn start(&mut self) -> (Self::Stop, Self::Wait) {
+        let input = self.fuzzer.generate_input();
+        let child = spawn_with_stdin(self.executable, input.as_bytes())
+            .expect("could not spawn child process");
+        let child = Arc::new(child);
+        (child.clone(), (child, input))
+    }
+}
 
-        Ok(ProgramResult {
-            stdout: "default output".to_string(),
-            stderr: "default error".to_string(),
-            exit_code: child.wait_timeout
-        })
+impl<T: Fuzzer + Send> Runner for DefaultRunner<T> {
+    fn run(&mut self) {
+        let mut searcher = StoppableLoop::new(RunnerLoopAction {
+            fuzzer: &mut self.fuzzer,
+            executable: &self.executable,
+        });
+
+        let stop = searcher.get_stop();
+        delay(self.timeout, stop);
+
+        let input_found = searcher.run();
+        match input_found {
+            Some(result) => println!("Execution succeeded: {}", result),
+            None => println!("Execution timed out"),
+        }
+    }
+
+    fn run_with_input(&mut self, _input: &str) -> Result<ProgramResult, String> {
+        todo!()
     }
 }
