cccfd3e8bce8f93b878970786b7dea009c1af8e4
Yonatan Reicher <yony252525@gmail.com>
2025-01-14 20:02:59 +0200

    Improve

--- resources/test_output_distribution/main.py
+++ resources/test_output_distribution/main.py
@@ -7,6 +7,12 @@ WORDS_PATH = THIS_DIR + "/words.txt"
 GENERATE_WORDS_CPP_PATH = THIS_DIR + "/generate_words.cpp"
 # Use EXE so it works both on Windows and Linux
 GENERATE_WORDS_EXE_PATH = THIS_DIR + "/generate_words.exe"
+BIG_LIST_OF_NAUGTHY_STRINGS_PATH = "./resources/big-list-of-naughty-strings.txt"
+
+
+def get_big_list_of_naugthy_strings() -> list[bytes]:
+    with open(BIG_LIST_OF_NAUGTHY_STRINGS_PATH, "rb") as f:
+        return f.read().splitlines()
 
 
 def compile_word_generator():
@@ -62,18 +68,20 @@ def generate_words():
     rm_word_generator()
 
 
-
-def read_words(file_path = WORDS_PATH) -> dict[bytes, int]:
+def read_words(file_path=WORDS_PATH) -> dict[bytes, int]:
     ret = {}
     count = 0
     with open(file_path, "rb") as f:
         try:
             while True:
                 len_bytes = f.read(8)
-                if len_bytes == b'': break
-                len = int.from_bytes(len_bytes, signed=False, byteorder='little')
+                if len_bytes == b'':
+                    break
+                len = int.from_bytes(
+                    len_bytes, signed=False, byteorder='little')
                 word = f.read(len)
-                if word not in ret: ret[word] = 0
+                if word not in ret:
+                    ret[word] = 0
                 ret[word] += 1
                 count += 1
         except:
@@ -81,12 +89,45 @@ def read_words(file_path = WORDS_PATH) -> dict[bytes, int]:
     return ret
 
 
+def prettify_word(word: bytes, max_chars: int | None = 150) -> str:
+    if max_chars is None:
+        word_str = None
+        try:
+            # a space to align with b'...'
+            word_str = ' "' + word.decode("utf-8") + '"'
+        except:
+            pass
+
+        return (
+            word_str
+            if word_str and word_str != '' and word_str.isprintable() else
+            str(word)
+        )
+
+    p = prettify_word(word, None)
+    if len(p) <= max_chars:
+        return p
+    return p[:max_chars - 3] + "..."
+
+
 def main():
     generate_words()
     words = read_words()
+    naughty_strings = set(get_big_list_of_naugthy_strings())
     sorted_by_values = sorted(words.items(), key=lambda item: item[1])
+
+    print("Naughty strings:")
     for word, count in sorted_by_values:
-        print(f"{count}: {word[:50]}")
+        if word not in naughty_strings:
+            continue
+        print(f"{count}: {prettify_word(word)}")
+
+    print("Our generated strings:")
+    for word, count in sorted_by_values:
+        if word in naughty_strings:
+            continue
+        print(f"{count}: {prettify_word(word)}")
+
 
 if __name__ == "__main__":
     main()
