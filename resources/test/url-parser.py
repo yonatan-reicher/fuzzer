1933a45df342745c7f8f187f08a2002b1e777ceb
Yonatan Reicher <yony252525@gmail.com>
2025-01-20 21:59:26 +0200

    Add URL parsers!

---
+++ resources/test/url-parser.py
@@ -0,0 +1,33 @@
+'''
+This is a URL parser to test our URL fuzzer!
+
+What is a URL? Ask [mozilla](https://developer.mozilla.org/en-US/docs/Learn_web_development/Howto/Web_mechanics/What_is_a_URL)
+'''
+
+
+def parse_and_print(url: str):
+    if '://' in url:
+        scheme, url = url.split('://')
+        print(f"Scheme: {scheme}")
+
+    if '#' in url:
+        url, anchor = url.split('#')
+        print(f"Anchor: {anchor}")
+    if '?' in url:
+        url, query = url.split('?')
+        print(f"Query: {query}")
+
+    if '/' in url:
+        authority, path = url.split('/', 1)
+        print(f"Domain: {authority}")
+        print(f"Path: {path}")
+
+
+def main():
+    url = input('Enter a URL: ')
+    parse_and_print(url)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ resources/test/urllib-parser.py
@@ -0,0 +1,11 @@
+#!python3
+
+from urllib.parse import urlparse
+
+
+if __name__ == "__main__":
+    url = input("Enter a URL: ")
+    parse_result = urlparse(url)
+    keys = parse_result._fields
+    for key in keys:
+        print(f"{key}: {getattr(parse_result, key)}")
