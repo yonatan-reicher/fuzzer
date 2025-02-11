9bd33aa78cba934cc8386a274f1774f9e6139cd2
Yonatan Reicher <yony252525@gmail.com>
2025-02-11 17:28:30 +0200

    Fix test

--- resources/test/crash-if-newline.py
+++ resources/test/crash-if-newline.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import sys
+
 
 if __name__ == "__main__":
     while True:
-        if '\n' in input():
+        if '\n' in sys.stdin.read()
             print('A newline????!!')
             exit(1)
